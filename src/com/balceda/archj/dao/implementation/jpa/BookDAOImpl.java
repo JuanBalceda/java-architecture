1c2271fa015ede46e616c7998808c54a0d1b0971
Juan Jesus Balceda Pomachagua <juanbalceda@gmail.com>
2018-10-17 01:04:20 +0200

    Chapter 18 - DRY & Spring Template

--- src/com/balceda/archj/dao/implementation/jpa/BookDAOImpl.java
+++ src/com/balceda/archj/dao/implementation/jpa/BookDAOImpl.java
@@ -2,9 +2,6 @@ package com.balceda.archj.dao.implementation.jpa;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-
 import com.balceda.archj.dao.exception.ExceptionDAO;
 import com.balceda.archj.dao.interfaces.BookDAO;
 import com.balceda.archj.model.Book;
@@ -17,30 +14,14 @@ public class BookDAOImpl extends GenericDAOImpl<Book, String> implements BookDAO
 
 	@Override
 	public List<Book> selectByCategory(Category category) throws ExceptionDAO {
-		EntityManager manager = getEmf().createEntityManager();
-
-		TypedQuery<Book> query = manager.createQuery("Select b from Book b where b.category=?1", Book.class);
-		query.setParameter(1, category);
-		List<Book> list = null;
-		try {
-			list = query.getResultList();
-		} finally {
-			manager.close();
-		}
-		return list;
+		return getJpaTemplate().find("Select b from Book b where b.category=?1", category);
 	}
+
 
 	@Override
 	public List<Book> selectAll() throws ExceptionDAO {
-		EntityManager manager = getEmf().createEntityManager();
-
-		TypedQuery<Book> query = manager.createQuery("Select b from Book b JOIN FETCH b.category", Book.class);
-		List<Book> list = null;
-		try {
-			list = query.getResultList();
-		} finally {
-			manager.close();
-		}
-		return list;
+		return getJpaTemplate().find("Select b from Book b JOIN FETCH b.category");
 	}
+
+
 }
--- src/com/balceda/archj/dao/implementation/jpa/GenericDAOImpl.java
+++ src/com/balceda/archj/dao/implementation/jpa/GenericDAOImpl.java
@@ -4,111 +4,42 @@ import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceException;
-import javax.persistence.TypedQuery;
+import org.springframework.orm.jpa.support.JpaDaoSupport;
 
 import com.balceda.archj.dao.exception.ExceptionDAO;
 import com.balceda.archj.dao.interfaces.GenericDAO;
 
-public class GenericDAOImpl<T,Id extends Serializable> implements GenericDAO<T, Id> {
+public class GenericDAOImpl<T, Id extends Serializable> extends JpaDaoSupport implements GenericDAO<T, Id> {
 
 	private Class<T> persistence;
-	private EntityManagerFactory emf;
 	
 	public GenericDAOImpl() {
-		this.persistence = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+		this.persistence = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass())
+				.getActualTypeArguments()[0];
 	}
 
 	@Override
 	public void insert(T t) throws ExceptionDAO {
-		EntityManager manager = getEmf().createEntityManager();
-		EntityTransaction transaction = null;
-		try {
-			transaction = manager.getTransaction();
-			transaction.begin();
-			manager.persist(t);
-			transaction.commit();
-		} catch (PersistenceException e) {
-			manager.getTransaction().rollback();
-			throw new ExceptionDAO();
-		} finally {
-			manager.close();
-		}		
+		getJpaTemplate().persist(t);
 	}
 
 	@Override
 	public List<T> selectAll() throws ExceptionDAO {
-		EntityManager manager = getEmf().createEntityManager();
-
-		TypedQuery<T> query = 
-				manager.createQuery("Select o from "+persistence.getSimpleName()+" o", persistence);
-		List<T> list = null;
-		try {
-			list = query.getResultList();
-		} finally {
-			manager.close();
-		}
-		return list;
+		return getJpaTemplate().find("Select o from " + persistence.getSimpleName() + " o");
 	}
 
 	@Override
 	public T selectById(Id id) throws ExceptionDAO {
-		EntityManager manager = getEmf().createEntityManager();
-
-		T object = null;
-		try {
-			object = (T) manager.find(persistence, id);
-		} finally {
-			manager.close();
-		}
-		return object;
+		return getJpaTemplate().find(persistence, id);
 	}
 
 	@Override
 	public void update(T t) throws ExceptionDAO {
-		EntityManager manager = getEmf().createEntityManager();
-
-		EntityTransaction transaction = null;
-		try {
-			transaction = manager.getTransaction();
-			transaction.begin();
-			manager.merge(t);
-			transaction.commit();
-		} catch (PersistenceException e) {
-			manager.getTransaction().rollback();
-			throw new ExceptionDAO();
-		} finally {
-			manager.close();
-		}		
+		getJpaTemplate().merge(t);
 	}
 
 	@Override
 	public void delete(T t) throws ExceptionDAO {
-		EntityManager manager = getEmf().createEntityManager();
-
-		EntityTransaction transaction = null;
-		try {
-			transaction = manager.getTransaction();
-			transaction.begin();
-			manager.remove(manager.merge(t));
-			transaction.commit();
-		} catch (PersistenceException e) {
-			manager.getTransaction().rollback();
-			throw new ExceptionDAO();
-		} finally {
-			manager.close();
-		}		
+		getJpaTemplate().remove(getJpaTemplate().merge(t));
 	}
-
-	public EntityManagerFactory getEmf() {
-		return emf;
-	}
-
-	public void setEmf(EntityManagerFactory emf) {
-		this.emf = emf;
-	}
-
 }
